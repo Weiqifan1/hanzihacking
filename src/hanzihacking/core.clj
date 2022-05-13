a66e53902c3d4de5dbf13885c897a780c8330a04
Weiqifan1 <cmlykke@hotmail.com>
2022-05-13 19:18:54 +0200

    2022-05-13_1918_tzaiAndJundaColliionListGenerated

--- src/hanzihacking/core.clj
+++ src/hanzihacking/core.clj
@@ -1,19 +1,20 @@
 (ns hanzihacking.core)
 (require '[clojure.java.io :as io])
 (require '[com.rpl.specter :as sp])
+(require '[clojure.data.json :as json])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; get tzai and junda maps
 
 (defn getFromTzai
   [pathStr]
-  (take 8000 (map #(clojure.string/trim (subs % 0 2)) (clojure.string/split (slurp (io/resource pathStr)) #"\r\n"))))
+  (take 13060 (map #(clojure.string/trim (subs % 0 2)) (clojure.string/split (slurp (io/resource pathStr)) #"\r\n"))))
 
 (defn getFromJunda
   [pathStr]
-  (take 8000 (map #(nth % 1) (map #(clojure.string/split % #"\t") (clojure.string/split (slurp (io/resource pathStr)) #"\r\n")))))
+  (take 9933 (map #(nth % 1) (map #(clojure.string/split % #"\t") (clojure.string/split (slurp (io/resource pathStr)) #"\r\n")))))
 
 (def tzaiChars (getFromTzai "rawFiles/tzai.txt"))
-;;(println (count tzaiChars))
+(println (count tzaiChars))
 ;;(println (last tzaiChars))
 
 (def jundaChars (getFromJunda "rawFiles/junda.txt"))
@@ -21,7 +22,7 @@
 ;;(println (take-last 5 jundaChars))
 
 (def tzaiFreqMap
-  (zipmap tzaiChars (take 8000 (range))))
+  (zipmap tzaiChars (take 13060 (range))))
 ;;(println (class (first tzaiChars)))
 ;;(println (take 10 tzaiFreqMap))
 ;;(println (get tzaiFreqMap "字"))
@@ -32,7 +33,7 @@
   (set jundaChars))
 
 (def jundaFreqMap
-  (zipmap jundaChars (take 8000 (range))))
+  (zipmap jundaChars (take 9933 (range))))
 ;;(println (class (first jundaChars)))
 ;;(println (take 10 jundaFreqMap))
 ;;(println (get jundaFreqMap "字"))
@@ -47,75 +48,81 @@
 (def zhengmaChars (getFromZhengma "rawFiles/zhengmaChar.txt"))
 ;(println (take 10 zhengmaChars))
 
+(defn strEndInVV [x]
+  (and (< 2 (count x)) (.endsWith x "vv")))
+(defn strEndInaaa [x]
+  (and (< 2 (count x)) (.endsWith x "aaa")))
+(defn strEndInaa [x]
+  (and (< 2 (count x)) (.endsWith x "aa")))
+(defn strEndIn [x text]
+  (and (< 2 (count x)) (.endsWith x text)))
+
+(defn removeIfPresent [x text]
+  (if (strEndIn x text) (subs x 0 (- (count x) (count text))) x))
+
+(defn removeTestFromColl [coll text tooSmall]
+  (map #(hash-map :code (if (< tooSmall (count (:code %)))
+                          (removeIfPresent (:code %) text)
+                          (:code %)) :freq (:freq %) :char (:char %)) coll))
+
 (def zhengmaRawFreqList
-  (map #(list
-          (nth % 0)
-          (get tzaiFreqMap (nth % 0))
-          (nth % 1)
-          ) zhengmaChars))
+       (filter #(some? (:freq %))
+               (map #(hash-map
+                       :char
+                       (nth % 0)
+                       :freq
+                       (if (some? (get tzaiFreqMap (nth % 0))) (inc (get tzaiFreqMap (nth % 0))) nil)
+                       :code
+                       (nth % 1)
+                       ) zhengmaChars)))        ;
 
-(def zhengmaCleanList
-  (group-by #(nth % 1)
-            (map #(list (nth % 0) (inc (nth % 1)) (nth % 2))
-                 (filter #(and (some? (nth % 1))
-                               (> 8001 (nth % 1))) zhengmaRawFreqList))))
+(defn removeBadStrings [coll]
+  (removeTestFromColl
+    (removeTestFromColl
+      (removeTestFromColl coll "vv" 2) "aaa" 3) "aa" 2))
+(def zhengmaSansBadStrings (removeBadStrings zhengmaRawFreqList))
+(println (count zhengmaSansBadStrings))
+;(println (take 10 zhengmaSansBadStrings))
 
-(def cleanZhengmaFreqSet
-  (set (map #(nth % 1) zhengmaCleanList)))
-;(println (class cleanZhengmaFreqSet))
-;(println (count cleanZhengmaFreqSet))
-;(println (count zhengmaRawFreqList))
-;(println (take 10 zhengmaCleanList))
-;(println (count zhengmaCleanList))
+(def zhengmaCollectedByFreq
+  (map #(hash-map :freq (nth % 0)
+                  :entries (nth % 1)) (group-by #(:freq %) zhengmaSansBadStrings)))
 
-(def zhengmaSortedClean
-  (map #(nth % 1) (sort-by #(nth % 0) zhengmaCleanList)))
-(println (take 50 zhengmaSortedClean))
+;(println (count zhengmaCollectedByFreq))
+;(println (take 10 zhengmaCollectedByFreq))
 
-(comment
-  (defn strEndInVV [x]
-    (and (< 2 (count x)) (.endsWith x "vv")))
-  (defn entryCodeEndsInVV [x]
-    (strEndInVV (nth x 2)))
-  (defn entryVetorHasEntryEndInVV [x]
-    (contains? (vec (map #(entryCodeEndsInVV %) x)) true)))
+(defn longestCodeLenth [entryVec]
+  (apply max (map #(count (:code %)) entryVec)))
+(defn retainLongCodes [entryVec]
+  (filter #(= (count (:code %)) (longestCodeLenth entryVec)) entryVec))
+(def zhengmaOnlyLongest
+  (sort-by :freq
+    (flatten
+      (map #(retainLongCodes %)
+           (map #(:entries %) zhengmaCollectedByFreq)))))
 
-(println (take 5 zhengmaSortedClean))
+(def zhengmaOnlyLongCollectedByFreq
+  (sort-by #(:freq (get (:entries %) 0)) (map #(hash-map :freq (nth % 0)
+                  :entries (nth % 1)) (group-by #(:freq %) zhengmaOnlyLongest))))
+(def zhengmaOnlyLongCollectedByCode
+  (sort-by #(:freq (get (:entries %) 0)) (map #(hash-map :code (nth % 0)
+                  :entries (nth % 1)) (group-by #(:code %) zhengmaOnlyLongest))))
+
+(println (count zhengmaOnlyLongCollectedByFreq))
+(println (take 10 zhengmaOnlyLongCollectedByFreq))
+(println (count zhengmaOnlyLongCollectedByCode))
+(println (take 10 zhengmaOnlyLongCollectedByCode))
+
+(def collisions (sort-by #(:freq (get (:entries %) 0)) (filter #(< 1 (count (:entries %))) zhengmaOnlyLongCollectedByCode)))
+(println (count collisions))
+(println (take 10 collisions))
+
+;;;;;;;;;;clojure print unicode
+(def testOutput (clojure.string/replace (.toString collisions) #"entries" "\n"))
+(spit "flubber2tzai13060.txt" testOutput)
 
 
 
-;(def data_v [1 5 7])
-;(println (sp/select sp/FIRST data_v))
 
-;(def testsp
-;  (map #(sp/select [(sp/walker string?)] %) zhengmaSortedClean))
-;(println (take 10 testsp))
 
-(comment
-  (defn codeSize [x] (count (nth x 2)))
-  (defn findMaxLen [x] (apply max (map #(codeSize %) x)))
-  (defn findMaxCodelist [x] (filter #(= (codeSize %) (findMaxLen x)) x))
-  (defn filterOnlyLargest [x] (map #(vector (nth % 0) (findMaxCodelist (nth % 1)))
-                                   x))
-  ;(println (take 18 (filterOnlyLargest zhengmaSortedClean)))
-  (def onlyLargest (filterOnlyLargest zhengmaSortedClean))
 
-  ;(def doubleCodes (filter #(< 1 (count (nth % 1))) onlyLargest))
-  ;(println (take 10 doubleCodes))
-  ;211
-
-  (def onlyLargeFlatList (map #(flatten (nth % 1)) onlyLargest))
-  (def seqOfCodes (seq (set (flatten (map #(nth % 2) onlyLargeFlatList)))))
-  ;(def mapOfCodes)
-
-  ;(def filterMathingCodes [x]
-  ;  (filter #(= x (nth % 2)) onlyLargeFlatList))
-
-  ;(println (take 10 onlyLargeFlatList))
-  ;(println (take 10 codeToEntries))
-  (defn filterMathingCodes [x] (filter #(= x (nth % 2)) onlyLargeFlatList))
-  (def codeToEntries (map #(filterMathingCodes %) seqOfCodes))
-
-  (def codeclashes (sort-by #(nth (nth % 0) 1) (filter #(< 1 (count %)) codeToEntries))))
-;429
-;;(println (take 10 codeclashes))
