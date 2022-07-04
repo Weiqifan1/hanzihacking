ed63cfd3142c2eb8a59281961fc952627530671b
Weiqifan1 <cmlykke@hotmail.com>
2022-07-04 21:01:07 +0200

    2022-07-04_2100_letterGalmostDone

--- src/hanzihacking/core.clj
+++ src/hanzihacking/core.clj
@@ -12,7 +12,7 @@
 ;(println (getAllIdsLine "門"))
 ;(println (zmWithExactCodeFromChar "門"))
 
-(def strToSearch "e")
+(def strToSearch "gq")
 (println (take 50 (getIdsElmsFromChar strToSearch)))
 (println (zmWithExactCharFromLetter strToSearch))
 (println (getExactIdElmFromChars strToSearch))
@@ -30,6 +30,21 @@
           "耒" "2Pck" "鬥" "2Pcc" "𡗗" "2Pco" "春" "2Sco"})
 (def zmD {"扌" "1Pd" "寸" "2Pds"}) ;missing 1Sd
 (def zmE {"卌" "1Te"
-          "艹" "1Pe" "廾" "1Se"}) ;missing 1Se
+          "艹" "1Pe" "廾" "1Se"
+          "十" "2Ped" "革" "2Pee"
+          "廿" "2Pea" "龷" "2Sea" "卌一" "2Sea" "⑤" "2Sea" ;[455 帶 eawl [⿳⑤冖巾]]
+          "甘" "2Peb" ;missin 2Seb
+          "其" "2Pec" "栽" "2Peh" "𢦏" "2Seh"
+          })
+(def zmF {"木" "1Pf" "酉" "2Pfd" "覀" "2Pfj" "西" "2Sfj"
+          "車" "2Pfk" "⑧" "2Sfk" ;[429 專 fkds [⿱⑧寸]]
+          "甫" "2Pfb" "雨" "2Pfv"
+          })
+(def zmG {"石" "1Pg" "丆" "1Sg" "厂" "2Pgg" "大" "2Pgd" "𠂇" "2Sgd"
+          "辰" "2Pgh" "尨" "2Sgm" ;the implified dragon harater is missing
+          "不" "2Pgi" "頁" "2Sgo" ;simplified 頁 is missin
+          "而" "2Pgl"
+          "豕" "2Pgq" "𧰨" "2Sgq" ;1 2Sgq mising
+          })
 
 ;壴   "丰" "2Sci"
